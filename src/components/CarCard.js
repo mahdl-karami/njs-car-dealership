65e64f6c0a580d6dfc1899322052b09387f0c3c6
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-10-31 15:06:07 +0330

    carPage done + style

--- src/components/CarCard.js
+++ src/components/CarCard.js
@@ -1,18 +1,21 @@
 //? import styles
 import styles from "@/styles/carCard.module.css";
+import Link from "next/link";
 const { card, lead } = styles;
 const CarCard = ({ car }) => {
-  const { image, name, model, year, distance, location, price } = car;
+  const { image, name, model, year, distance, location, price, id } = car;
   return (
-    <div className={card}>
-      <img src={image} />
-      <h3>{name + " " + model}</h3>
-      <p className={lead}>{year + " . " + distance + "Km"}</p>
-      <span>
-        <p>{"$ " + String(price)}</p>
-        <p>{location}</p>
-      </span>
-    </div>
+    <Link href={`/cars/${id}`}>
+      <div className={card}>
+        <img src={image} />
+        <h3>{name + " " + model}</h3>
+        <p className={lead}>{year + " . " + distance + "Km"}</p>
+        <span>
+          <p>{"$ " + String(price)}</p>
+          <p>{location}</p>
+        </span>
+      </div>
+    </Link>
   );
 };
 
--- src/pages/cars/[carId].js
+++ src/pages/cars/[carId].js
@@ -1,9 +1,49 @@
-import React from 'react'
+import cars from "@/data/carsData";
+import { useRouter } from "next/router";
+import styles from "@/styles/carPage.module.css";
+const { page, desc } = styles;
 
 const Car = () => {
+  const router = useRouter();
+  const carId = router.query.carId;
+  const car = cars.filter((car) => car.id == carId)[0];
+  const { image, name, model, year, distance, description, location, price } = car;
   return (
-    <div>Car</div>
-  )
-}
+    <div className="wrapper">
+      <div className={page}>
+        {" "}
+        <img src={image} />
+        <h3>{name + " " + model}</h3>
+        <div>
+          <span>
+            <p>Company</p>
+            <p>Model</p>
+            <p>First registration</p>
+            <p>km/s driven</p>
+          </span>
+          <span>
+            <p>{name}</p>
+            <p>{model}</p>
+            <p>{year}</p>
+            <p>{distance}</p>
+          </span>
+        </div>
+        <div>
+          <p>location</p>
+          <p>{location}</p>
+        </div>
+        <div className={desc}>
+          <h3>Extra Information</h3>
+          <p>{description}</p>
+        </div>
+        <div>
+          <p>Price : </p>
+          <p>{price}</p>
+        </div>
+        <button>Buy</button>
+      </div>
+    </div>
+  );
+};
 
-export default Car
+export default Car;
