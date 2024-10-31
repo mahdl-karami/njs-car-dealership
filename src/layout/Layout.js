7a29b9b28cf7e7b72a4a3c409d9ad69c14fdf64d
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-10-31 11:41:50 +0330

    add layout + style

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,11 +1,26 @@
-import React from "react";
+//? import styles
+import styles from "@/styles/layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <header>header</header>
+      <header className={styles.header}>
+        <div className="wrapper">
+          <h1>Car Dealership</h1>
+          <p>Choose and buy your car ...</p>
+        </div>
+      </header>
       {children}
-      <footer>footer</footer>
+      <footer className={styles.footer}>
+        <div className="wrapper">
+          <p>
+            Developer by{" "}
+            <a target="_BLANK" href="https://github.com/mahdl-karami">
+              mahdl-karami
+            </a>
+          </p>
+        </div>
+      </footer>
     </>
   );
 };
