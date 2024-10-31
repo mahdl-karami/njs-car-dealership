5070b909462f23114230beaad31cec60702ba2ad
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-10-31 14:22:42 +0330

    render all cars category

--- src/pages/cars/index.js
+++ src/pages/cars/index.js
@@ -1,7 +1,19 @@
-import React from "react";
+import CarCard from "@/components/CarCard";
+import cars from "@/data/carsData";
 
 const Cars = () => {
-  return <div>Cars</div>;
+  return (
+    <div className="wrapper">
+      <h2 className="title">All Cars</h2>
+      <div className="grid">
+        {cars.map((car, i) => (
+          <div key={i}>
+            <CarCard car={car} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Cars;
