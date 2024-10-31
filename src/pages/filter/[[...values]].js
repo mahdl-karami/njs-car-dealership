a5e3b76aa7c40a33161f51b358744b4f44a10c0a
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-10-31 14:44:33 +0330

    filter by price done

--- src/pages/filter/[[...values]].js
+++ src/pages/filter/[[...values]].js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { useRouter } from "next/router";
+import cars from "@/data/carsData";
+import CarCard from "@/components/CarCard";
 const Filter = () => {
-  return (
-    <div>Filter</div>
-  )
-}
+  const router = useRouter();
+  const queries = router.query?.values;
+  const min = queries[0];
+  const max = queries[1];
 
-export default Filter
+  const filteredCars = cars.filter((car) => car.price > min && car.price < max);
+
+  return (
+    <div className="wrapper">
+      <h2 className="title">
+        Price filter : {min}$ to {max}$
+      </h2>
+      <div className="grid">
+        {filteredCars.map((car, i) => (
+          <div key={i}>
+            <CarCard car={car} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
