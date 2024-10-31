4f4f1b906b2209a3e9623ebb40104e4008e3cfa3
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-10-31 14:29:47 +0330

    filter by categories done

--- src/data/categories.js
+++ src/data/categories.js
@@ -2,7 +2,7 @@ const categories = [
   { name: "All", path: "/cars" },
   { name: "Sedan", path: "/categories/sedan" },
   { name: "SUV", path: "/categories/suv" },
-  { name: "Hackback", path: "/categories/hackback" },
+  { name: "Hatchback", path: "/categories/hatchback" },
   { name: "Sport", path: "/categories/sport" },
 ];
 export default categories;
--- src/pages/categories/[[...category]].js
+++ src/pages/categories/[[...category]].js
@@ -1,7 +1,26 @@
-import React from "react";
+import { useRouter } from "next/router";
+import cars from "@/data/carsData";
+import CarCard from "@/components/CarCard";
 
 const Category = () => {
-  return <div>Category</div>;
+  const router = useRouter();
+  const queries = router.query;
+
+  const x = cars.filter((car) => car.category == queries.category[0]);
+
+  console.log(x);
+  return (
+    <div className="wrapper">
+      <h2 className="title">{queries.category[0]}</h2>
+      <div className="grid">
+        {x.map((car, i) => (
+          <div key={i}>
+            <CarCard car={car} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Category;
